367a11e283460fdb3b1c1b262a6ab13bdfcbf2fd
Max Newport <newpomax@gmail.com>
2022-05-24 11:56:05 -0700

    New formulation works? kinda

--- iLQR.py
+++ iLQR.py
@@ -6,11 +6,16 @@ import matplotlib.pyplot as plt
 from tqdm.auto import tqdm
 from functools import partial
 
+
 def regularize(A: jnp.ndarray, λ:float):
     w,v = jnp.linalg.eig(A)
     w = jnp.real(w)
-    w = jnp.diag(jnp.maximum(jnp.zeros_like(w), w) + λ)
-    return w, jnp.real(v) # return eig matrix, eigenvec matrix
+    w = jnp.maximum(jnp.zeros_like(w), w) + λ
+    winv = 1/w
+    w = jnp.diag(w)
+    winv = jnp.diag(winv)
+    v = jnp.real(v)
+    return winv, v # return inverse eig matrix, eigenvec matrix
 
 @partial(jax.jit, static_argnums=(0,))
 # @partial(jax.vmap, in_axes=(None, 0, 0))
@@ -22,7 +27,8 @@ def linearize_dynamics(fd: callable,
         f(s,u) = A@[dx,1].T+B@du 
     """
     fd_s, fd_u = jax.jacfwd(fd, (0,1))(s,u)
-    
+    A = jnp.array(fd_s) + jnp.eye(fd_s.shape[0])
+    B = jnp.array(fd_u)
     return A, B
 
 @partial(jax.jit, static_argnums=(0,))
@@ -38,29 +44,32 @@ def quadratize_cost(c: callable,
     dc = jax.jacfwd(c, (0,1))
     cs, cu = dc(s,u,t)
     (css, csu), (cus, cuu) = jax.jacfwd(dc,(0,1))(s,u,t)
-    ck = jnp.array(c(s,u,t),ndmin=2)
-    cs = jnp.array(cs,ndmin=2).T 
-    cu = jnp.array(cu,ndmin=2).T
-    Q = jnp.vstack((jnp.hstack((css, cs)),jnp.hstack((cs.T, 2*ck))))
-    R = cuu
-    S = jnp.vstack((csu,cu.T))
+    Q = jnp.array(css)
+    θ = jnp.array(cs,ndmin=2).T
+    q = jnp.array(c(s,u,t),ndmin=2)
+    R = jnp.array(cuu)
+    r = jnp.array(cu,ndmin=2).T
+    P = jnp.array(cus)
     
-    return Q,R,S
+    return Q,θ,q,R,r,P
+
+def rk4(f,s,u,dt):
+    k1 = dt * f(s, u)
+    k2 = dt * f(s + k1 / 2, u)
+    k3 = dt * f(s + k2 / 2, u)
+    k4 = dt * f(s + k3, u)
+    return (k1 + 2 * k2 + 2 * k3 + k4) / 6
 
 def discretize(f,dt):
     """ Make discretized dynamics function from discrete function, `f(s,u)` with time step `dt` 
         using Runge-Kutta 4th order method.
     """
-    def integrator(s, u, dt=dt):
-        k1 = dt * f(s, u)
-        k2 = dt * f(s + k1 / 2, u)
-        k3 = dt * f(s + k2 / 2, u)
-        k4 = dt * f(s + k3, u)
-        return s + (k1 + 2 * k2 + 2 * k3 + k4) / 6
+    def integrator(s, u, f=f, dt=dt):
+        return rk4(f,s,u,dt)
     
     return integrator
-    
-def LQR_iteration(fd: callable, c: callable, N: int, u_bar:np.ndarray, s_bar:np.ndarray, s0: np.ndarray, u_range:np.ndarray, λ:float, dt:float):
+
+def LQR_iteration(fd: callable, c: callable, h: callable, N: int, u_bar:np.ndarray, s_bar:np.ndarray, s0: np.ndarray, u_range:np.ndarray, λ:float, dt:float, εmax:float = 1 ):
     """ Solve a single LQR sub-problem with discretized dynamics `fd(s,u)`, cost function
         `c(s,u,t)`, reference linear control law `F` for augmented delta dynamics, reference control `u_bar`,
         reference trajectory `s_bar`, initial state `s0`, and Levenberg-Marquardt constant λ.
@@ -68,27 +77,38 @@ def LQR_iteration(fd: callable, c: callable, N: int, u_bar:np.ndarray, s_bar:np.
         Returns the linear feedback law at each time step for the augmented dynamics vector,
         [s, 1].T
     """
-    P = np.zeros((N, s0.size + 1, s0.size + 1))
-    P[-1] = quadratize_cost(c, s_bar[N-1], u_bar[N-1],dt*(N-1))[0] # initialize P matrix for Ricatti recursion, augmented for affine dynamics/cost
-    L = np.zeros((N, u_bar.shape[1], s0.size + 1)) # initialize output linear control matrix for augmented dynamics
-    for i in range(N-2,-1,-1):
+    S = np.zeros((N+1, s0.size, s0.size))
+    ς = np.zeros((N+1, s0.size, 1))
+    s = np.zeros((N+1, 1, 1))
+    S[-1], ς[-1], s[-1] = quadratize_cost(lambda s,u,t: h(s), s_bar[N], jnp.zeros_like(u_bar[0]), dt*(N))[:3] # initialize P matrix for Ricatti recursion, augmented for affine dynamics/cost
+    L = np.zeros((N, u_bar.shape[1], s0.size)) # initialize output linear control matrix 
+    l = np.zeros((N, u_bar.shape[1], 1)) # initialize output linear control affine term
+    for i in range(N-1,-1,-1):
         A, B = linearize_dynamics(fd, s_bar[i], u_bar[i])
-        Q, R, S = quadratize_cost(c, s_bar[i], u_bar[i],i*dt)
-        D, V = regularize(R+B.T@P[i+1]@B, λ) # Levenberg-Marquardt-esque regularization
-        Hinv = V@(1/D)@V.T # compute quick inverse using eigen decomp values from regularization
-        L[i] = -Hinv@(B.T@P[i+1]@A + S.T) # linear control law for augmented delta-dynamics
-        for j in range(u_bar.shape[1]):
-            if u_bar[i,j] <= u_range[j,0] or u_bar[i,j] >= u_range[j,1]:
-                L[i,j,:] = 0 # control constraints impact choice of optimal L
-        P[i] = Q + A.T@P[i+1]@A + (A.T@P[i+1]@B + S)@L[i] # quadratic cost matrix for augmented delta-dynamics
+        Q, θ, q, R, r, P = quadratize_cost(c, s_bar[i], u_bar[i], i*dt)
 
-    new_s_bar, new_u_bar = simulate(fd, L, u_bar, s_bar, s0, N, u_range) # forward pass - simulate reference control foward using augmented dynamics
-    cost = 0
-    for i in range(N): # compute cost
-        cost += c(new_s_bar[i], new_u_bar[i],dt*i)
-    return L, new_u_bar, new_s_bar, cost
+        H = R + B.T@S[i+1]@B
+        G = P + B.T@S[i+1]@A
+        g = r + B.T@ς[i+1]
+        Dinv, V = regularize(H, λ) # Levenberg-Marquardt-esque regularization
+        ℋinv = V@Dinv@V.T # compute quick inverse using eigen decomp values from regularization
+        dustar = -ℋinv@g
+        ε = min(1,εmax)
+        # ε = jnp.amin(jnp.clip((u_range[:,1]- u_bar[i])/dustar,0,1))
+        ε = min(ε, jnp.amin(jnp.clip((u_range[:,0]- u_bar[i])/dustar,0,1)))
+        l[i] = ε*dustar
+        L[i] = -ℋinv@G 
+        for j in range(u_bar.shape[1]): # for each dimension of control
+            if (l[i,j] + u_bar[i,j]) <= u_range[j,0] or (l[i,j] + u_bar[i,j]) >= u_range[j,1]:
+                L[i,j,:] = 0 # control constraints impact choice of optimal L
+        S[i] = Q + A.T@S[i+1]@A + L[i].T@H@L[i] + L[i].T@G + G.T@L[i] # quadratic cost matrix for augmented delta-dynamics
+        ς[i] = θ + A.T@ς[i+1] + L[i].T@g + G.T@l[i]
+        s[i] = q + s[i+1] + 0.5*l[i].T@H@l[i] + l[i].T@g
+
+    new_s_bar, new_u_bar = simulate(fd, L, l, u_bar, s_bar, s0, N, u_range) # forward pass - simulate reference control foward using augmented dynamics
+    return L, new_u_bar, new_s_bar, np.squeeze(s[0])
     
-def simulate(fd:callable, L: np.ndarray, u_bar: np.ndarray, s_bar: np.ndarray, s0: np.ndarray, N:int, u_range:np.ndarray):
+def simulate(fd:callable, L: np.ndarray, l:np.ndarray, u_bar: np.ndarray, s_bar: np.ndarray, s0: np.ndarray, N:int, u_range:np.ndarray):
     """ Simulate the results of a linear control matrix `F` for `N` time steps via the discretized 
         delta dynamics function 'fd(s,u)' given an initial state, `s0`. 
     """
@@ -96,37 +116,38 @@ def simulate(fd:callable, L: np.ndarray, u_bar: np.ndarray, s_bar: np.ndarray, s
     u = np.zeros((N,u_bar.shape[1]))
     s[0] = s0
     for i in range(N):
-        ds = np.array(s[i]-s_bar[i],ndmin=2).T # control law is based on distance from reference, s_bar
-        u[i] = L[i]@jnp.vstack((ds,[[1]])) + u_bar[i] # output of control is difference from reference, u_bar
+        ds = jnp.array(s[i]-s_bar[i],ndmin=2).T # control law is based on distance from reference, s_bar
+        u[i] = jnp.squeeze( L[i]@ds + l[i] + jnp.array(u_bar[i],ndmin=2).T ) # output of control is difference from reference, u_bar
         u[i] = jnp.clip(u[i],u_range[:,0],u_range[:,1])  # impose control contraint
-        s[i+1] = fd(s[i],u[i])
+        s[i+1] = s[i] + fd(s[i],u[i]) # use discretized function to integrate
     return s, u
 
-def iLQR(f:callable, c:callable, u_ref: np.ndarray, N: int, s0: np.ndarray, u_range:np.ndarray,
+def iLQR(f:callable, c:callable, h:callable, u_ref: np.ndarray, N: int, s0: np.ndarray, u_range:np.ndarray,
          dt:float = 1E-2, max_iters:int = 400, tol: float = 1E-4, λmax:float= 1E4 ):
     """ Solve the iLQR problem for a system with discrete dynamics `f(s,u)`, cost function
-        `c(s,u)`, initial reference control `uref`, number of time steps `N+1`, initial state `s0`,
+        `c(s,u)`, terminal cost `h(s)`, initial reference control `uref`, number of time steps `N+1`, initial state `s0`,
         time step `dt`, max number of iterations 'max_iters', and convergence tolerance `tol`.
     """
     fd = jax.jit(discretize(f,dt))
-    # fd = f
     # Create initial reference trajectory from initial reference control, and compute its cost
     s_bar = np.zeros((N+1,s0.size))
     u_bar = u_ref
-    cost = 0
+    cost = np.inf
     s_bar[0] = s0
+    
+    # do initial simulation
     for i in range(N):
-        s_bar[i+1] = fd(s_bar[i],u_bar[i])
-        cost += c(s_bar[i],u_bar[i],i*dt)
+        s_bar[i+1] = s_bar[i] + fd(s_bar[i],u_bar[i]) 
                 
-    λbase = 1E-4
+    λbase = 1E-8
     λfactor = 10
     λ = λbase
-    u_dif = np.inf
+    u_dif = -1
+    new_cost = -1
     print('Reference cost = %0.2e' % cost)
     for k in ( pbar := tqdm(range(max_iters)) ):
-        pbar.set_description("λ = %.2e, u_dif = %.2e, best cost-so-far = %0.2e" % (λ,u_dif,cost))
-        F, new_u_bar, new_s_bar, new_cost = LQR_iteration(fd, c, N, u_bar, s_bar, s0, u_range, λ, dt)
+        pbar.set_description("λ = %.2e, u_dif = %.2e, last cost = %.5e, best cost-so-far = %0.5e" % (λ,u_dif,new_cost,cost))
+        F, new_u_bar, new_s_bar, new_cost = LQR_iteration(fd, c, h, N, u_bar, s_bar, s0, u_range, λ, dt, 1)
         u_dif = np.amax(np.linalg.norm((u_bar - new_u_bar),np.inf,axis=1))
         if new_cost < cost and u_dif < tol: # if cost has converged 
             print("iLQR converged after {0} iterations with cost {1:.2e}.".format(k+1,cost))
--- springdamper_iLQRtest.py
+++ springdamper_iLQRtest.py
@@ -12,11 +12,11 @@ def springdamp(s, u):
     """Compute the spring-damp state derivative."""
     m = 1.     # mass
     k = 10   # spring coeff
-    b = 1 # damping coeff
+    b = 0 # damping coeff
     g = 9.81    # gravitational acceleration
-    xeq = 1 # spring equilibrium
+    xeq = 0 # spring equilibrium height
     x, v = s
-    a = u/m - (b/m)*v - (k/m)*(x-xeq)  # spring damper in gravity
+    a = u/m - (b/m)*v - (k/m)*(x-xeq) - g # spring damper in gravity
     ds = jnp.array([v, a[0]])
     return ds
 
@@ -27,11 +27,12 @@ s0 = np.array([2, 0])  # initial
 period = 2
 sgoal = lambda t: jnp.array([2*(jnp.cos(2*np.pi*t/period)**2),0])
 dt = 0.05                             # discrete time resolution
-u_range = np.array([[-10, 10]])
+u_range = np.array([[-100, 100]])
 T = 10.                              # total simulation time
-Q = np.eye(n)                   # state cost matrix
+Q = 10*np.eye(n)                   # state cost matrix
 R = np.eye(m)                   # control cost matrix
 c = lambda s,u,t: (s-sgoal(t)).T@Q@(s-sgoal(t))+ u@R@u   # cost function
+h = lambda s: (s-sgoal(T)).T@Q@(s-sgoal(T))
 t = np.arange(0., T + dt, dt)
 N = t.size - 1
 
@@ -39,7 +40,7 @@ N = t.size - 1
 u_ref = 0.01*(u_range[0,1]-u_range[0,0])*rng.random((N,m)) - u_range[0,0]
 u_ref = 5*np.array([np.cos(2*np.pi*t[:-1]/period)]).T
 
-F,u,s = iLQR.iLQR(springdamp, c, u_ref, N, s0, u_range, dt)
+F,u,s = iLQR.iLQR(springdamp, c, h, u_ref, N, s0, u_range, dt)
 
 plt.plot(t,s[:,0])
 plt.show()
